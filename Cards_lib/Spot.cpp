06bbb7a6b37b1e1075ec6e9fa01acaf9ab0740c6
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2026-09-16 14:33:25 +0200

    add spot class

---
+++ Cards_lib/Spot.cpp
@@ -0,0 +1,35 @@
+﻿//
+// Created by Stefan on 16-Sep-26.
+//
+
+#include "Spot.h"
+
+#include <algorithm>
+#include <random>
+#include <vector>
+
+#include "Card.h"
+
+using namespace std;
+
+Spot::Spot() {
+    m_cards = vector<Card*> {};
+}
+
+vector<Card *> Spot::getCards() {
+    return m_cards;
+}
+
+void Spot::addCard(Card &card) {
+    m_cards.emplace_back(&card);
+}
+
+void Spot::shuffleCards() {
+    shuffle(m_cards.begin(), m_cards.end(), std::mt19937(std::random_device()()));
+}
+
+Card & Spot::removeTopCard() {
+    auto card = m_cards.back();
+    m_cards.pop_back();
+    return *card;
+}
---
+++ Cards_lib/Spot.h
@@ -0,0 +1,25 @@
+﻿//
+// Created by Stefan on 16-Sep-26.
+//
+
+#ifndef SPECIALISTERNEBLACKJACK_SPOT_H
+#define SPECIALISTERNEBLACKJACK_SPOT_H
+#include <vector>
+
+#include "Card.h"
+
+
+class Spot {
+private:
+    std::vector<Card*> m_cards;
+public:
+    Spot();
+
+    std::vector<Card*> getCards();
+    void addCard(Card &card);
+    void shuffleCards();
+    Card& removeTopCard();
+};
+
+
+#endif //SPECIALISTERNEBLACKJACK_SPOT_H

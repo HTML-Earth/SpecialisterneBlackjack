21095c410126226990e1d69254e8a60fad5e4b18
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2026-09-18 10:35:33 +0200

    more tests

--- Catch_tests/BlackjackTests.cpp
+++ Catch_tests/BlackjackTests.cpp
@@ -21,9 +21,32 @@ TEST_CASE("Game starts properly", "[blackjack]") {
     REQUIRE(game->getHouseHand().getCards().size() == 2);
 }
 
+TEST_CASE("Game will end after standing", "[blackjack]") {
+    for (int i = 0; i < 100; i++) {
+        auto game = new TestableBlackjackGame();
+        REQUIRE_NOTHROW(game->startGame());
+        REQUIRE_NOTHROW(game->stand());
+
+        REQUIRE(game->getCurrentState() == BlackjackGame::ended);
+    }
+}
+
+TEST_CASE("Game will have ended after drawing ten cards", "[blackjack]") {
+    for (int i = 0; i < 100; i++) {
+        auto game = new TestableBlackjackGame();
+        REQUIRE_NOTHROW(game->startGame());
+        for (int c = 0; c < 10; c++) {
+            REQUIRE_NOTHROW(game->hit());
+        }
+        REQUIRE(game->getCurrentState() == BlackjackGame::ended);
+    }
+}
+
 TEST_CASE("Hit draws card", "[blackjack]") {
     auto game = new TestableBlackjackGame();
     REQUIRE_NOTHROW(game->startGame());
+    REQUIRE(game->getPlayerStayed() == false);
+    REQUIRE(game->getPlayerHand().getCards().size() == 2);
     REQUIRE_NOTHROW(game->hit());
     REQUIRE(game->getPlayerStayed() == false);
     REQUIRE(game->getPlayerHand().getCards().size() == 3);
@@ -32,7 +55,23 @@ TEST_CASE("Hit draws card", "[blackjack]") {
 TEST_CASE("Stand does not draw card", "[blackjack]") {
     auto game = new TestableBlackjackGame();
     REQUIRE_NOTHROW(game->startGame());
+    REQUIRE(game->getPlayerStayed() == false);
+    REQUIRE(game->getPlayerHand().getCards().size() == 2);
     REQUIRE_NOTHROW(game->stand());
     REQUIRE(game->getPlayerStayed() == true);
     REQUIRE(game->getPlayerHand().getCards().size() == 2);
+}
+
+TEST_CASE("Can't hit when game is inactive", "[blackjack]") {
+    auto game = new TestableBlackjackGame();
+    REQUIRE(game->getPlayerHand().getCards().size() == 0);
+    REQUIRE_NOTHROW(game->hit());
+    REQUIRE(game->getPlayerHand().getCards().size() == 0);
+}
+
+TEST_CASE("Can't stand when game is inactive", "[blackjack]") {
+    auto game = new TestableBlackjackGame();
+    REQUIRE(game->getPlayerStayed() == false);
+    REQUIRE_NOTHROW(game->stand());
+    REQUIRE(game->getPlayerStayed() == false);
 }
